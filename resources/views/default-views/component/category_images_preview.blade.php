0a52102a1a9827f1ab70315a98d1d3d03a25f8ea
qiweihan <234151994@qq.com>
2019-02-19 02:49:46 +0800

    还原

--- resources/views/default-views/component/category_images_preview.blade.php
+++ resources/views/default-views/component/category_images_preview.blade.php
@@ -33,7 +33,6 @@
 </style>
 <div class="container cate-preview">
     <div class="cate-head d-flex">
-        {{dd($cate)}}
         <h3 class="cate-name float-left">{{$cate['name']}}</h3>
         <div class="d-flex child-cates">
             @foreach($cate['childs'] as $child_cate)
