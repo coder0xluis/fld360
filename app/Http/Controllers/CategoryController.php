ac3cbace790aa1240eedb4dba6b7c2a70b4d9426
qiweihan <234151994@qq.com>
2019-02-19 04:30:44 +0800

    类目排序处理

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,26 +11,28 @@ class CategoryController extends Controller
     {
         $categories = DB::table('category')
             ->where('is_deleted', 0)
-            ->get();
-//            ->keyBy('id');
+//            ->orderBy('sort','asc')
+//            ->orderByRaw('sort=0', 'asc', 'sort')
+            ->get()
+            ->keyBy('id');
+//        dd($categories);
 
 
         foreach ($categories as $cate) {
+//            dd($cate);
             $cate_id = $cate->id;
             if ($cate->parent_id) { //子类目
                 $cates[$cate->parent_id]['childs'][$cate_id] = collect($cate)->toArray();
                 $cates[$cate->parent_id]['cate_ids'][] = $cate->id;
             } else {    //父类目
                 $cates[$cate_id] = collect($cate)->toArray();
+
                 $cates[$cate_id]['cate_ids'][] = $cate_id;
             }
         }
+//        dd($cates);
 
-        return collect($cates)->sortBy(function ($item) {
-            if ($item['sort'] == 0) {
-                return 100;
-            }
-        })->values()->toArray();
+        return $cates;
     }
 
 
