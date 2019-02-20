7e58a07747e3b4c7ac618dd7a7925cae5d7733bf
qiweihan <234151994@qq.com>
2019-02-20 14:08:10 +0800

    googleAnalytics

--- resources/views/default-views/component/base.blade.php
+++ resources/views/default-views/component/base.blade.php
@@ -8,8 +8,20 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description"
           content="海量福利图片每日更新：{{collect($categories)->implode('name',',')}}"/>
-    <meta name="google-site-verification" content="poQyj7ya9cVNTQjcgrVTYBMZt-syMKSpk9gRqyG24jA" />
+    <meta name="google-site-verification" content="poQyj7ya9cVNTQjcgrVTYBMZt-syMKSpk9gRqyG24jA"/>
     <link rel="stylesheet" href="/css/app.css">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134838157-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-134838157-1');
+    </script>
 
     <title>@yield('title') - 福利岛</title>
 </head>
