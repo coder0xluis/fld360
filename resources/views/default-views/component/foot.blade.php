903b414976fe6f49053e71e9ac74769f6e37b595
qiweihan <234151994@qq.com>
2019-02-21 23:59:20 +0800

    添加mailto快捷邮件方式

--- resources/views/default-views/component/foot.blade.php
+++ resources/views/default-views/component/foot.blade.php
@@ -1,7 +1,7 @@
 <div class="footer bg-dark text-secondary pt-4 overflow-hidden font-weight-light">
     <div class="container">
         <p>版权声明：本网站部分内容来源于互联网，如有侵犯版权请来信告知，我们将立即处理。</p>
-        <p>站长邮箱：0xluis2019@gmail.com</p>
+        <p>站长邮箱：<a class="text-white" href="mailto:0xluis2019@gmail.com">0xluis2019@gmail.com</a></p>
         <p class="">&copy; {{\Carbon\Carbon::now()->year}}. All rights reserved.</p>
     </div>
 </div>
