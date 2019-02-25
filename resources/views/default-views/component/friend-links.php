11d2cb0c2ac986a4484a7ce9a1ec35c12cae7bd1
qiweihan <234151994@qq.com>
2019-02-26 00:12:32 +0800

    友情链接nofollow

--- resources/views/default-views/component/friend-links.php
+++ resources/views/default-views/component/friend-links.php
@@ -6,6 +6,6 @@
 <div class="friend-links mt-4 mb-4">
     <h5 class="">友情链接</h5>
     <div class="mt-2 d-block">
-        <a class="mr-2 mt-1 d-inline-block" href="http://id88.me" target="_blank">视频VIP在线解析</a>
+        <a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://id88.me" target="_blank">视频VIP在线解析</a>
     </div>
 </div>
