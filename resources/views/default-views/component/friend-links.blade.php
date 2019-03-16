3de794b11f7aaac4f59497968dc067eefed4138a
qiweihan <234151994@qq.com>
2019-03-17 00:44:54 +0800

    新增友链

--- resources/views/default-views/component/friend-links.blade.php
+++ resources/views/default-views/component/friend-links.blade.php
@@ -8,6 +8,10 @@
         <h5 class="">友情链接</h5>
         <div class="mt-2 d-block">
             <a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://id88.me" target="_blank">视频VIP在线解析</a>
+            |
+            <a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://hxctf.com/?AgentID=38100" target="_blank">
+                含羞草-成人短片
+            </a>
         </div>
     </div>
 </div>
