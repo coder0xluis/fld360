a369e35742abe3ecef5b1289a7baf573f253150a
qiweihan <234151994@qq.com>
2019-03-26 16:23:20 +0800

    删除友链

--- resources/views/default-views/component/friend-links.blade.php
+++ resources/views/default-views/component/friend-links.blade.php
@@ -8,7 +8,7 @@
         <h5 class="">友情链接</h5>
         <div class="mt-2 d-block">
             <a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://id88.me" target="_blank">视频VIP在线解析</a>
-            <a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://www.uunot.com" target="_blank">迪丽热巴黑丝</a>
+            {{--<a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://www.uunot.com" target="_blank">迪丽热巴黑丝</a>--}}
         </div>
     </div>
 </div>
