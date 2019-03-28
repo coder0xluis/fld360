db8174f675b00451665ff55153947d9689e20ad0
qiweihan <234151994@qq.com>
2019-03-28 18:30:10 +0800

    新增友链

--- resources/views/default-views/component/friend-links.blade.php
+++ resources/views/default-views/component/friend-links.blade.php
@@ -7,6 +7,7 @@
     <div class="container">
         <h5 class="">友情链接</h5>
         <div class="mt-2 d-block">
+            <a class="mr-2 mt-1 d-inline-block" href="https://goodlucky.me" target="_blank">幸运电影网</a>
             <a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://id88.me" target="_blank">视频VIP在线解析</a>
             {{--<a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://www.uunot.com" target="_blank">迪丽热巴黑丝</a>--}}
         </div>
