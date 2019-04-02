380cdf98f356d1573dd6f6fe2b4d181638be5b46
qiweihan <234151994@qq.com>
2019-04-03 05:20:27 +0800

    新增友链

--- resources/views/default-views/component/friend-links.blade.php
+++ resources/views/default-views/component/friend-links.blade.php
@@ -8,6 +8,7 @@
         <h5 class="">友情链接</h5>
         <div class="mt-2 d-block">
             <a class="mr-2 mt-1 d-inline-block" href="https://goodlucky.me" target="_blank">幸运电影网</a>
+            <a class="mr-2 mt-1 d-inline-block" href="https://smm2019.com" target="_blank">色妹妹在线视频</a>
             <a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://id88.me" target="_blank">视频VIP在线解析</a>
             {{--<a rel="nofollow" class="mr-2 mt-1 d-inline-block" href="http://www.uunot.com" target="_blank">迪丽热巴黑丝</a>--}}
         </div>
